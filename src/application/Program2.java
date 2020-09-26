77e62985920bdb900e7273eab0fa0c0b29ebf492
willps1 <willianps1@gmail.com>
2020-09-26 11:43:18 -0300

    Implements findById and program

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,9 +1,18 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		Department department = departmentDao.findById(2);
+		
+		System.out.println(department);
 
 	}
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -11,7 +11,7 @@ public class DaoFactory {
 	}
 	
 	public static DepartmentDao createDepartmentDao() {
-		return new DepartmentDaoJDBC();
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
